1741c388d0ca68f2247d4b9cd55e00aebf7b4f36
tildeslash <info@tildeslash.com>
2018-04-25 16:41:06 +0200

    fix coverity CID #273526

--- src/http/cervlet.c
+++ src/http/cervlet.c
@@ -933,6 +933,10 @@ static void do_viewlog(HttpRequest req, HttpResponse res) {
 
 static void handle_service(HttpRequest req, HttpResponse res) {
         char *name = req->url;
+        if (! name) {
+                send_error(req, res, SC_NOT_FOUND, "Service name required");
+                return;
+        }
         Service_T s = Util_getService(++name);
         if (! s) {
                 send_error(req, res, SC_NOT_FOUND, "There is no service named \"%s\"", name ? name : "");
