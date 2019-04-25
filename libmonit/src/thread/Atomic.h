3e19c48f342e20a2e60fe1ab49a9801348fc1ef7
Tildeslash <info@tildeslash.com>
2019-04-25 04:39:37 +0200

    Replace license header in Atomic.h

--- libmonit/src/thread/Atomic.h
+++ libmonit/src/thread/Atomic.h
@@ -1,6 +1,25 @@
 /*
- * Copyright (C) 2007-2019 Tildeslash Ltd. All rights reserved.
- * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Copyright (C) Tildeslash Ltd. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ *
+ * You must obey the GNU Affero General Public License in all respects
+ * for all of the code used other than OpenSSL.
  */
 
 
@@ -32,6 +51,8 @@
  * This is a legacy interface for simple atomic operations and should be
  * removed when C11 _Atomic becomes available on all supported platforms.
  *
+ * @author http://www.tildeslash.com/
+ * @see http://www.mmonit.com/
  * @file
  */
 
