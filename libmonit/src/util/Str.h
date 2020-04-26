d733fd7df40eec2e83a9a00f5f654437b96a7681
Tildeslash <info@tildeslash.com>
2020-04-26 14:38:59 +0200

    Use bool instead of int and int64_t instead of long long

--- libmonit/src/util/Str.h
+++ libmonit/src/util/Str.h
@@ -130,7 +130,7 @@ int Str_parseInt(const char *s);
  * @exception NumberFormatException If the String does not contain a
  * parsable long long
  */
-long long int Str_parseLLong(const char *s);
+int64_t Str_parseLLong(const char *s);
 
 
 /**
@@ -165,7 +165,7 @@ char *Str_replaceChar(char *s, char o, char n);
  * @param b The sub-string to test a against
  * @return true if a starts with b, otherwise false
  */
-int Str_startsWith(const char *a, const char *b);
+bool Str_startsWith(const char *a, const char *b);
 
 
 /**
@@ -176,7 +176,7 @@ int Str_startsWith(const char *a, const char *b);
  * @param b The sub-string to test a against
  * @return true if a ends with b, otherwise false
  */
-int Str_endsWith(const char *a, const char *b);
+bool Str_endsWith(const char *a, const char *b);
 
 
 /**
@@ -211,7 +211,7 @@ char *Str_sub(const char *a, const char *b);
  * @param s The string to test
  * @return true if s contains chars in charset, otherwise false
  */
-int Str_has(const char *charset, const char *s);
+bool Str_has(const char *charset, const char *s);
 
 
 /**
@@ -239,7 +239,7 @@ char *Str_unescape(const char *charset, char *s);
  * @param b The string to test for equality with <code>a</code>
  * @return true if a equals b, otherwise false
  */
-int Str_isEqual(const char *a, const char *b);
+bool Str_isEqual(const char *a, const char *b);
 
 
 /**
@@ -249,7 +249,7 @@ int Str_isEqual(const char *a, const char *b);
  * @param b The string to test for equality with <code>a</code>
  * @return true if a equals b, otherwise false
  */
-int Str_isByteEqual(const char *a, const char *b);
+bool Str_isByteEqual(const char *a, const char *b);
 
 
 /**
@@ -383,7 +383,7 @@ char *Str_curtail(char *s, char *t);
  * @return true if <code>s.length > limit</code> otherwise false
  * @exception AssertException if limit is less than 0
  */
-int Str_lim(const char *s, int limit);
+bool Str_lim(const char *s, int limit);
 
 
 /**
@@ -401,7 +401,7 @@ int Str_lim(const char *s, int limit);
  * @exception AssertException if pattern is invalid or cannot be 
  * compiled. 
  */
-int Str_match(const char *pattern, const char *subject);
+bool Str_match(const char *pattern, const char *subject);
 
 
 /**
@@ -411,7 +411,7 @@ int Str_match(const char *pattern, const char *subject);
  * @return A hash value for the String
  * @see Table.h and Set.h
  */
-unsigned int Str_hash(const void *x);
+int Str_hash(const void *x);
 
 
 /**
