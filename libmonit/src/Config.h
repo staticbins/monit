ef1a12e9f77c9e86c7fbd566eb1029886bf110dd
Tildeslash <info@tildeslash.com>
2020-04-26 14:21:00 +0200

    Include C99 toolchain interfaces, <stdbool.h> for using bool instead of our own boolean_t type and <stdint.h> for using explicit numerical width types where applicable. Note that we should not go overboard with using specific int width types and use the standard int type wherever we can. I.e. don't use int32_t, if the standard int type suffice.

--- libmonit/src/Config.h
+++ libmonit/src/Config.h
@@ -42,12 +42,8 @@
 #include <limits.h>
 #include <string.h>
 #include <sys/types.h>
-#ifdef HAVE_MACH_BOOLEAN_H
-#include <mach/boolean.h>
-#endif
-#ifdef HAVE_UVM_UVM_PARAM_H
-#include <uvm/uvm_param.h>
-#endif
+#include <stdint.h>
+#include <stdbool.h>
 
 #include "assert.h"
 #include "system/Mem.h"
@@ -76,22 +72,13 @@
 #include "NumberFormatException.h"
 
 
-/* ------------------------------------------ General Purpose value macros */
+/* ------------------------------------------------------- Constant macros */
 
 
 /**
  * Standard String length
  */
 #define STRLEN 256
-
-
-/* ---------------------------------------------------------- Build macros */
-
-
-/* Mask out GCC __attribute__ extension for non-gcc compilers. */
-#ifndef __GNUC__
-#define __attribute__(x)
-#endif
 
 
 /* ------------------------------------------------------ Type definitions */
@@ -102,28 +89,6 @@
  */
 #ifndef HAVE_UCHAR_T
 typedef unsigned char uchar_t;
-#endif
-
-
-/**
- * The internal 32 bits integer type
- */
-#ifndef HAVE_UINT32_T
-typedef unsigned int uint32_t;
-#endif
-
-
-/**
- * The internal boolean integer type
- */
-#ifndef HAVE_BOOLEAN_T
-typedef enum {
-        false = 0,
-        true
-} __attribute__((__packed__)) boolean_t;
-#else
-#define false 0
-#define true  1
 #endif
 
 
