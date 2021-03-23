1e05b9237def21671e56eef2ee69736941184cb7
tildeslash <info@tildeslash.com>
2021-03-23 19:26:19 +0100

    fix coverity CID 369639 Unchecked return value

--- src/http/processor.c
+++ src/http/processor.c
@@ -521,7 +521,8 @@ static void send_response(HttpRequest req, HttpResponse res) {
                         Socket_print(S, "%s", headers);
                 Socket_print(S, "\r\n");
                 if (bodyLength)
-                        Socket_write(S, (unsigned char *)body, bodyLength);
+                        if(Socket_write(S, (unsigned char *)body, bodyLength) < 0)
+                                Log_error("Http: Cannot send the response -- %s\n", STRERROR);
                 FREE(headers);
         }
 }
