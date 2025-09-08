3152d1443ebcf8b51ec793813ca35c85a2ad4539
tildeslash <info@tildeslash.com>
2025-09-09 00:17:27 +0200

    Fixed: The MongoDB protocol test did support only MongodDB < 5.1. The protocol test was updated to support the newer mongodb ping style.

--- src/protocols/mongodb.c
+++ src/protocols/mongodb.c
@@ -58,9 +58,10 @@ typedef struct {
         int32_t operation;       // operation type
         // OP_REPLY
         int32_t flags;           // flags
-        long long cursorId;        // cursor id
+        long long cursorId;      // cursor id
         int32_t cursorStart;     // cursor start
         int32_t returned;        // returned documents count
+        int32_t documentLength;  // document length
         unsigned char *response; // BSON encoded response object
 } op_reply_t; /* Keep position, don't memory pack! */
 
@@ -79,22 +80,27 @@ static unsigned int B4(unsigned char *b) {
 
 
 static void _ping(Socket_T socket) {
-        unsigned char ping[58] = {
+        unsigned char ping[68] = {
                 // Message header
-                0x3a, 0x00, 0x00, 0x00,                                           // total message size (58 bytes)
+                0x44, 0x00, 0x00, 0x00,                                           // total message size (68 bytes)
                 0x01, 0x00, 0x00, 0x00,                                           // message id (1)
-                0xff, 0xff, 0xff, 0xff,                                           // response to id (not used in request)
+                0x00, 0x00, 0x00, 0x00,                                           // response to id (0)
                 0xd4, 0x07, 0x00, 0x00,                                           // operation type (OP_QUERY = 2004)
                 // Query
                 0x00, 0x00, 0x00, 0x00,                                           // flags
                 0x61, 0x64, 0x6d, 0x69, 0x6e, 0x2e, 0x24, 0x63, 0x6d, 0x64, 0x00, // db.collection C-string ("admin.$cmd")
                 0x00, 0x00, 0x00, 0x00,                                           // number of documents to skip
-                0xff, 0xff, 0xff, 0xff,                                           // number of documents to return
-                // BSON encoded PING command: {ping:1}
-                0x13, 0x00, 0x00, 0x00,                                           // total document length (19 bytes)
-                0x01,                                                             // element type (double = 0x1)
-                0x70, 0x69, 0x6e, 0x67, 0x00,                                     // element name C-string ("ping")
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f,                   // element value (1)
+                0xff, 0xff, 0xff, 0xff,                                           // number of documents to return (-1)
+                // BSON encoded document
+                0x1d, 0x00, 0x00, 0x00,                                           // total document length (29 bytes)
+                // ismaster (executes the legacy topology discovery command)
+                0x10,                                                             // element type (int32 = 0x10)
+                0x69, 0x73, 0x6d, 0x61, 0x73, 0x74, 0x65, 0x72, 0x00,             // element name C-string ("ismaster")
+                0x01, 0x00, 0x00, 0x00,                                           // element value (1)
+                // helloOk (indicates support for the newer hello command)
+                0x08,                                                             // element type (bool = 0x08)
+                0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x4f, 0x6b, 0x00,                   // element name C-string ("helloOk")
+                0x01,                                                             // element value (true = 1)
                 0x00                                                              // BSON document terminal
         };
         if (Socket_write(socket, (unsigned char *)ping, sizeof(ping)) < 0)
@@ -108,6 +114,12 @@ static void _pong(Socket_T socket) {
         memset(&pong, 0, sizeof(op_reply_t));
         if (Socket_read(socket, buf, 16) < 16) // read the header
                 THROW(IOException, "MONGODB: error receiving PING response -- %s", STRERROR);
+        // Get message size (should have at minimum 51 bytes)
+        pong.messageSize = B4(buf);
+        if (pong.messageSize < 51)
+                THROW(ProtocolException, "MONGODB: PING response error -- invalid size (%d)", pong.messageSize);
+        // read message ID (ignore)
+        pong.messageId = B4(buf + 4);
         // check response ID: should be 1 (hardcoded in _ping request above)
         pong.responseToId = B4(buf + 8);
         if (pong.responseToId != 1)
@@ -117,20 +129,31 @@ static void _pong(Socket_T socket) {
         if (pong.operation != 1)
                 THROW(ProtocolException, "MONGODB: PING response error -- unexpected operation type (0x%x)", pong.operation);
         // read OP_REPLY
-        pong.messageSize = B4(buf);
         int len = pong.messageSize - 16 > STRLEN ? STRLEN : pong.messageSize - 16; // Adjust message size for this buffer (minus 16 bytes of header - already read)
         if (Socket_read(socket, buf, len) != len)
                 THROW(IOException, "MONGODB: error receiving OP_REPLY data -- %s", STRERROR);
-        // check BSON encoded OK response: {ok:1}
-        pong.response = buf + 20;
-        unsigned char ok[17] = {
-                0x11, 0x00, 0x00, 0x00,                         // total document length (17 bytes)
-                0x01,                                           // element type (double = 0x1)
-                0x6f, 0x6b, 0x00,                               // element name C-string ("ok")
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f, // element value (1)
-                0x00                                            // BSON document terminal
+        // read flags (ignore)
+        pong.flags = B4(buf);
+        // read cursor id (ignore)
+        pong.cursorId = B4(buf + 4);
+        // read cursor start (ignore)
+        pong.cursorStart = B4(buf + 12);
+        // read returned documents count
+        pong.returned = B4(buf + 16);
+        if (pong.returned < 1)
+                THROW(ProtocolException, "MONGODB: PING response error -- unexpected documents count (%d)", pong.returned);
+        // read document length (should have at minimum 11 bytes)
+        pong.documentLength = B4(buf + 20);
+        if (pong.documentLength < 11)
+                THROW(ProtocolException, "MONGODB: PING response error -- unexpected documents length (%d)", pong.documentLength);
+        // compare just the first element in the response (we expect helloOk=true)
+        pong.response = buf + 24;
+        unsigned char helloOk[10] = {
+                0x08,                                           // element type (boolean = 0x8)
+                0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x4f, 0x6b, 0x00, // element name C-string ("helloOk")
+                0x01                                            // element value (true = 1)
         };
-        if (memcmp(pong.response, ok, sizeof(ok)))
+        if (memcmp(pong.response, helloOk, sizeof(helloOk)))
                 THROW(ProtocolException, "MONGODB: PING response error -- invalid reply");
 }
 
